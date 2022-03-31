fff504417c5b7874d69488650200885b7ebb9f4e
redouredou <eljarrai.reda.pro@gmail.com>
2022-03-31 19:16:39 +0200

    add second unit test to Grid class about lengthY

--- src/grid/Grid.spec.ts
+++ src/grid/Grid.spec.ts
@@ -1,11 +1,12 @@
 import Grid from "./Grid"
 
-
-
 describe('Grid tests', () => {
 
-    /*My first acceptance*/ 
     it('should throw IllegalArgument when grid x length is negative', () => {
         expect(() => new Grid(-10, 20)).toThrow('you cannot have an x length negative');
+    })
+
+    it('should throw IllegalArgument when grid y length is negative', () => {
+        expect(() => new Grid(10, -20)).toThrow('you cannot have an y length negative');
     })
 })
--- src/grid/Grid.ts
+++ src/grid/Grid.ts
@@ -4,6 +4,9 @@ export default class Grid {
     private lengthY: number;
 
     constructor(lengthX: number, lengthY: number){
+        if(lengthY < 0){
+            throw new Error('you cannot have an y length negative')
+        }
         if(lengthX < 0){
             throw new Error('you cannot have an x length negative')
         }
